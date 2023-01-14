5938ce1f863dc1706858bad74f1c22bb91117776
Andre Granemann <andre.granemann@proway.treina>
2023-01-13 21:01:28 -0300

    partials

--- aplicacao/alunos/views.py
+++ aplicacao/alunos/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+    
+def aluno(request):
+    return render(request,'aluno.html')
