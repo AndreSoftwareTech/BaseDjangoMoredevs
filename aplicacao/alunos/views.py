e519e63ab78fc68a383ab5272461003c71488ab5
Andre Granemann <andre.granemann@proway.treina>
2023-01-16 21:06:17 -0300

    conectando banco/ models

--- aplicacao/alunos/views.py
+++ aplicacao/alunos/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    alunos = {
+        1:'Lirinha',
+        2:'Felipe',
+        3:'Marcos'
+    }
+    dados ={
+        'nome_do_aluno' : alunos 
+    }
+    return render(request,'index.html', dados)
     
 def aluno(request):
     return render(request,'aluno.html')
--- aplicacao/moredevs/settings.py
+++ aplicacao/moredevs/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'moredevs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'teste',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5435',
     }
 }
 
