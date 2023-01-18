9a2dc6e74762f39ee9d8feb26b7abd79748cf19b
Andre Granemann <andre.granemann@proway.treina>
2023-01-18 20:13:33 -0300

    finalizacao modulo django

--- aplicacao/alunos/views.py
+++ aplicacao/alunos/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Alunos
 
 
 # Create your views here.
 def index(request):
-    alunos = {
-        1:'Lirinha',
-        2:'Felipe',
-        3:'Marcos'
-    }
+    alunos = Alunos.objects.all()
+
     dados ={
-        'nome_do_aluno' : alunos 
+        'alunos' : alunos 
     }
+
     return render(request,'index.html', dados)
+
     
-def aluno(request):
-    return render(request,'aluno.html')
+def aluno(request, aluno_id):
+    alunos = get_object_or_404(Alunos, pk=aluno_id )
+
+    aluno_a_exibir = {
+        'aluno': alunos
+    }
+
+    return render(request,'aluno.html', aluno_a_exibir)
